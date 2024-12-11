47b8ccc0c447ee48f50cd15c05963d5ece5071e4
KAMRUZZAMAN <kamrulcse1@gmail.com>
2024-12-11 21:24:30 +0600

    Assignment-2
    Assignment-2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,8 +16,14 @@ class ProfileController extends Controller
             'age' => $age,
         ];
 
-        $cookie = cookie('access_token', '123-XYZ', 1, '/', $_SERVER['SERVER_NAME'], false, true);
-        
-        return response($data, 200)->withCookie($cookie);
+        $name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response($data, 200)->cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
     }
 }
